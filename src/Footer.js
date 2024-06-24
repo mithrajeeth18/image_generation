3e547f0c50265160ea0070db02200dc746b2840e
AryanRai12 <aryanrai4647@gmail.com>
2024-06-24 12:41:59 +0530

    Footer added

---
+++ src/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Footer.css';
+import logo from './logoJio.svg.png'; // Update the path as per your structure
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-left">
+          <img src={logo} alt="Jio Logo" className="footer-logo" />
+          <span className="footer-text">
+            Copyright © 2024 Reliance Jio Infocomm Ltd. All rights reserved.
+          </span>
+        </div>
+        <div className="footer-links">
+          <a href="#" className="footer-link">Press release</a>
+          <a href="#" className="footer-link">Regulatory</a>
+          <a href="#" className="footer-link">Policies</a>
+          <a href="#" className="footer-link">Terms & conditions</a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
