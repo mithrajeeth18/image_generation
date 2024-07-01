542da8b3c8ca58b775c4c300d020bc1c8ce10322
Mithrajeeth18 <mithra86753@gmail.com>
2024-07-01 17:30:09 +0530

    delete functionality

--- src/DragAndDrop.js
+++ src/DragAndDrop.js
@@ -1,24 +1,40 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useRef, useEffect } from "react";
 import "./DragAndDrop.css";
 import { fabric } from "fabric";
 
-const DragAndDropWithText = ({ textProperties, setCanvas }) => {
+const DragAndDropWithText = ({ setCanvas }) => {
   const canvasRef = useRef(null);
   const fabricCanvasRef = useRef(null);
 
   useEffect(() => {
     const canvasElement = canvasRef.current;
     const fabricCanvas = new fabric.Canvas(canvasElement, {
-      backgroundColor: 'white', // Set canvas background to white
-      //backgroundColor: "#640209",
+      backgroundColor: "white", // Set canvas background to white
       width: 700,
       height: 500,
     });
     fabricCanvasRef.current = fabricCanvas;
     setCanvas(fabricCanvas); // Set the canvas reference
 
+    // Event listener for keydown
+    const handleKeyDown = (e) => {
+      if (
+        (e.key === "Delete" || e.key === "Backspace") &&
+        (e.metaKey || e.ctrlKey)
+      ) {
+        const activeObject = fabricCanvas.getActiveObject();
+        if (activeObject) {
+          fabricCanvas.remove(activeObject);
+          fabricCanvas.renderAll();
+        }
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
     return () => {
       fabricCanvas.dispose();
+      document.removeEventListener("keydown", handleKeyDown);
     };
   }, [setCanvas]);
 
