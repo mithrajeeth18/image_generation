28882d475aff9056e7fdb8effb731da24b35556c
Mithrajeeth18 <mithra86753@gmail.com>
2024-07-17 17:13:36 +0530

    logos name added

--- Frontend/src/LogosPanel.js
+++ Frontend/src/LogosPanel.js
@@ -1,16 +1,25 @@
 // src/components/LogosPanel.js
 
-import React from 'react';
-import './LogosPanel.css'; // Create and import a CSS file for styling
+import React from "react";
+import "./LogosPanel.css"; // Create and import a CSS file for styling
 
 const LogosPanel = ({ logos, onSelectLogo }) => {
   return (
     <div className="logos-panel">
-      {logos.map((logo, index) => (
-        <div key={index} className="logo-item" onClick={() => onSelectLogo(logo)}>
-          <img src={logo} alt={`Logo ${index + 1}`} />
-        </div>
-      ))}
+      {logos.map((logo, index) => {
+        // Extract the file name from the logo URL
+        const fileName = logo.split("/").pop().split(".")[0];
+        return (
+          <div
+            key={index}
+            className="logo-item"
+            data-hover-text={fileName}
+            onClick={() => onSelectLogo(logo)}
+          >
+            <img src={logo} alt={`Logo ${index + 1}`} />
+          </div>
+        );
+      })}
     </div>
   );
 };
